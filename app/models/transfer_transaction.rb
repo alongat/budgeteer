79ad3e481e850d032487cd90fce580c46bcaa63b
alongat <alon@skycure.com>
2019-12-31 21:34:21 +0200

    Add transfer transaction

---
+++ app/models/transfer_transaction.rb
@@ -0,0 +1,7 @@
+# typed: false
+# frozen_string_literal: true
+
+class TransferTransaction < ApplicationRecord
+  belongs_to :from_account, class_name: 'Account', foreign_key: 'from_account_id'
+  belongs_to :to_account, class_name: 'Account', foreign_key: 'to_account_id'
+end
---
+++ db/migrate/20191228171937_create_transfer_transactions.rb
@@ -0,0 +1,17 @@
+class CreateTransferTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transfer_transactions do |t|
+      t.datetime :date, null: false
+      t.float :amount, null: false
+      t.string :note
+
+      t.timestamps
+    end
+
+    add_column :transfer_transactions, :from_account_id, :integer, default: 0, null: false
+    add_index :transfer_transactions, :from_account_id
+
+    add_column :transfer_transactions, :to_account_id, :integer, default: 0, null: false
+    add_index :transfer_transactions, :to_account_id
+  end
+end
